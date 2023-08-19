655511b21f80f04084dbbcef3804cc9322546c50
HelinSahin <helin13cuma@gmail.com>
2023-08-20 00:45:10 +0300

    ikinci düzeltme

--- js/main.js
+++ js/main.js
@@ -49,28 +49,7 @@
 
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-
+  
 
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
